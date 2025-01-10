c21d206f99cf25a72af8625db1904f77d1eb9ead
Anindit Gopalakrishnan <aninditg98@gmail.com>
2025-01-10 20:58:02 +0000

    isoalte

--- apps/protein_folding/helixfold3/isolate_msa_issue.py
+++ apps/protein_folding/helixfold3/isolate_msa_issue.py
@@ -10,11 +10,11 @@ def try_creating_msa_features(folder):
     uniref90_msa = parsers.parse_stockholm(get_text(os.path.join(folder, 'uniref90_hits.sto')))
     mgnify_msa = parsers.parse_stockholm(get_text(os.path.join(folder, 'mgnify_hits.sto')))
     bfd_msa = parsers.parse_stockholm(get_text(os.path.join(folder, 'small_bfd_hits.sto')))
-    msa_features = make_msa_features((uniref90_msa, bfd_msa, mgnify_msa))
+    make_msa_features((uniref90_msa, bfd_msa, mgnify_msa))
 
 if __name__ == '__main__':
-    # internal_xtal_folder = "/home/anindit/paddle-helix-fork/apps/protein_folding/helixfold3/output/row-0/msas/protein_A/A"
-    posebuster_folder = "/home/anindit/deep-affinity/experimental/users/anindit/posebuster_5SAK_ZRY/sto"
-    msa_features = try_creating_msa_features(posebuster_folder)
+    failed_posebuster_folder = "/home/anindit/deep-affinity/experimental/users/anindit/posebuster_5SAK_ZRY/sto"
+    working_posebuster_folder = "/home/anindit/paddle-helix-fork/apps/protein_folding/helixfold3/output/posebuster_5SAK_ZRY/msas/protein_A/A-HF3"
+    msa_features = try_creating_msa_features(failed_posebuster_folder)
     
     
